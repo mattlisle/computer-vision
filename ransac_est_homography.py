c07d973b9e85e4fca8f0ceb0c962bb90a46aab58
mattlisle <matthew.j.lisle@gmail.com>
2018-10-29 17:54:26 -0700

    First go at RANSAC

--- ransac_est_homography.py
+++ ransac_est_homography.py
@@ -20,9 +20,33 @@ def ransac_est_homography(x1, y1, x2, y2, thresh):
   from est_homography import est_homography
   
   # Number of RANSAC trials
-  t = 100
+  t = 1000
 
   # Length of matching points arrays
   n = len(x1)
+  z = np.ones(n)
+
+  inlier_ind = np.array([])
+  for i in range(n):
+
+    # Choose 4 points randomly and estimate a homography
+    choices = np.random.choice(n, 4)
+    H = est_homography(x1[choices], y1[choices], x2[choices], y2[choices])
+
+    # Use the homography to transform points from img1 to estimated postions in img2
+    estimates = np.matmul(H, np.stack([x1, y1, z]))
+    
+    # Normalize the estimates and extract x and y
+    estimates = estimates / estimates[-1]
+    x_est = estimates[0]
+    y_est = estimates[1]
+
+    # Compute sum of squared distances (That's what the assignment said, but I don't see why we would sum them?)
+    distances = np.sqrt(np.square(x2 - x_est) + np.square(y2 - y_est))
+    ind = np.where(distances < thresh)
+
+    # If we did better this time, save the indices
+    if len(ind) > len(inlier_ind):
+      inlier_ind = ind
 
   return H, inlier_ind
--- team_shatt_wrapper.py
+++ team_shatt_wrapper.py
@@ -60,12 +60,16 @@ matches1 = np.where([match >= 0])[1].astype(int)
 matches2 = match[match >= 0].astype(int)
 
 print("Performing RANSAC")
-x1 = x1[matches1]
-x2 = x2[matches2]
-y1 = y1[matches1]
-y2 = y2[matches2]
+x1m = x1[matches1]
+x2m = x2[matches2]
+y1m = y1[matches1]
+y2m = y2[matches2]
 thresh = 20
-H, inlier_ind = ransac_est_homography(x1, y1, x2, y2, thresh)
+H, inlier_ind = ransac_est_homography(x1m, y1m, x2m, y2m, thresh)
+xin1 = x1m[inlier_ind]
+xin2 = x2m[inlier_ind]
+yin1 = y1m[inlier_ind]
+yin2 = y2m[inlier_ind]
 
 # plt.imshow(img2)
 # plt.scatter(x2, y2)
@@ -76,21 +80,21 @@ H, inlier_ind = ransac_est_homography(x1, y1, x2, y2, thresh)
 
 ##########
 # Plot both images with interest points side by side
-fig, (left, right) = plt.subplots(1, 2, sharey=True)
+# fig, (left, right) = plt.subplots(1, 2, sharey=True)
 
-left.imshow(img1)
-left.scatter(x1, y1)
+# left.imshow(img1)
+# left.scatter(x1, y1)
 # for i in range(boxes1.shape[2]):
 # 	left.plot(boxes1[:,0,i], boxes1[:,1,i], color="red")
 # 	left.plot(oris1[0,:,i], oris1[1,:,i], color="green")
 
-right.imshow(img2)
-right.scatter(x2, y2)
+# right.imshow(img2)
+# right.scatter(x2, y2)
 # for i in range(boxes2.shape[2]):
 # 	right.plot(boxes2[:,0,i], boxes2[:,1,i], color="red")
 # 	right.plot(oris2[0,:,i], oris2[1,:,i], color="green")
 
-plt.show()
+# plt.show()
 ##########
 
 # fig, (left, right) = plt.subplots(1, 2, sharey=True)
@@ -108,23 +112,38 @@ plt.show()
 # 	right.plot(oris2[0,:,i], oris2[1,:,i], color="green")
 # plt.show()
 
-both = np.concatenate((img1, img2), axis=1)
-mx = np.zeros(2)
-my = np.zeros(2)
+##########
+# Plot the corresponding points before RANSAC
+# both = np.concatenate((img1, img2), axis=1)
+# mx = np.zeros(2)
+# my = np.zeros(2)
 
-plt.imshow(both)
-plt.scatter(x1[matches1], y1[matches1])
-plt.scatter(x2[matches2] + img2.shape[1], y2[matches2], color="C0")
-for i in matches1:
-	plt.plot(boxes1[:,0,i], boxes1[:,1,i], color="red")
-	plt.plot(oris1[0,:,i], oris1[1,:,i], color="green")
+# plt.imshow(both)
+# plt.scatter(x1[matches1], y1[matches1])
+# plt.scatter(x2[matches2] + img2.shape[1], y2[matches2], color="C0")
+# for i in matches1:
+# 	plt.plot(boxes1[:,0,i], boxes1[:,1,i], color="red")
+# 	plt.plot(oris1[0,:,i], oris1[1,:,i], color="green")
 	
-for j, i in enumerate(matches2):
-	plt.plot(boxes2[:,0,i] + img2.shape[1], boxes2[:,1,i], color="red")
-	plt.plot(oris2[0,:,i] + img2.shape[1], oris2[1,:,i], color="green")
-	mx = [x1[matches1[j]], x2[i] + img1.shape[1]]
-	my = [y1[matches1[j]], y2[i]]
+# for j, i in enumerate(matches2):
+# 	plt.plot(boxes2[:,0,i] + img2.shape[1], boxes2[:,1,i], color="red")
+# 	plt.plot(oris2[0,:,i] + img2.shape[1], oris2[1,:,i], color="green")
+# 	mx = [x1[matches1[j]], x2[i] + img1.shape[1]]
+# 	my = [y1[matches1[j]], y2[i]]
+# 	plt.plot(mx, my, color="orange")
+
+
+# plt.show()
+##########
+
+##########
+# Plot inliers from RANSAC
+both = np.concatenate((img1, img2), axis=1)
+plt.imshow(both)
+plt.scatter(xin1, yin1)
+plt.scatter(xin2 + img1.shape[1], yin2, color="C0")
+for i in inlier_ind:
+	mx = [x1m[i], x2m[i] + img1.shape[1]]
+	my = [y1m[i], y2m[i]]
 	plt.plot(mx, my, color="orange")
-
-
 plt.show()
